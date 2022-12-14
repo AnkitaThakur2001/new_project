0ffccd23fa3273f2050b0dd573ce0f1fbfe7c40d
AnkitaThakur2001 <pro@Pros-iMac.local>
2022-12-14 18:59:15 +0530

    code push

--- app/src/main/java/com/app/newprojectrepo/utils/UserSharedPrefrences.kt
+++ app/src/main/java/com/app/newprojectrepo/utils/UserSharedPrefrences.kt
@@ -1,4 +1,18 @@
 package com.app.newprojectrepo.utils
 
 class UserSharedPrefrences {
+    private val TOKEN = "token"
+    private val ISLOGIN = "islogin"
+    private val ROLE = "role"
+    private val COMPANYNAME = "companyName"
+    private val ISDATAFILLED = "isDataFilled"
+    private val PAGE = "page"
+    private val USERID = "userid"
+    private val CONTACT = "contactnumber"
+    private val ISSUBSCRIBED = "isSubscribed"
+    private val USERNAME = "username"
+    private val ISANSGIVEN = "isansgiven"
+    private val SUBINDUSTRYID = "subIndustryID"
+    private val PASSWORD = "password"
+    private val EMPLOYERNAME = "employerName"
 }
