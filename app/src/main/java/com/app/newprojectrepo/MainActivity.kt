8c3a381b853471774b17aee2e671a71bfc5cf2a8
Pro <pro@Pros-iMac.local>
2022-12-14 12:15:19 +0530

    anku

--- app/src/main/java/com/app/newprojectrepo/MainActivity.kt
+++ app/src/main/java/com/app/newprojectrepo/MainActivity.kt
@@ -6,10 +6,20 @@ import com.app.newprojectrepo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    private val repository = Repository()
+    private var RC_SIGN_IN = 100
+    private var socialEmail: String? = null
+     private var socialName: String? = null
+    private var ankita:String?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
+    private fun init(){
 
+    }
+
+    private fun setObserver(){
+    }
 }
