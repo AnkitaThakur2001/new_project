683626a3ed2bfdc3ab05318775f44611da9f6c19
AnkitaThakur2001 <pro@Pros-iMac.local>
2022-12-14 15:48:23 +0530

    some changes

---
+++ app/src/main/java/com/app/newprojectrepo/ui/MyActivity.kt
@@ -0,0 +1,12 @@
+package com.app.newprojectrepo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.newprojectrepo.R
+
+class MyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my)
+    }
+}
